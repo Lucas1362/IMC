9350bba71624e14d7fb41145b17b4f87cf6cde07
Jamerson Lucas Tenorio Valentim <jamersontenorio88@gmail.com>
2025-12-22 17:25:09 -0300

    Organizacao por camadas

---
+++ JavaScript/script.js
@@ -0,0 +1,5 @@
+alert("Olá, bem-vindo à calculadora de IMC!");
+function calcularIMC() {
+    var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value) / 100;
+}
