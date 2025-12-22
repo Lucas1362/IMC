d81f765ac3840d780aa6ec2f94f660f2f9714415
Jamerson Lucas Tenorio Valentim <jamersontenorio88@gmail.com>
2025-12-22 18:39:28 -0300

    teste de implementação de mensagem de erro ou acerto

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,26 +3,28 @@ function calcularIMC(event) {
 
     // Previne o comportamento padrão do formulário
     event.preventDefault();
-
     const peso = parseFloat(document.getElementById("peso").value);
     const altura = parseFloat(document.getElementById("altura").value);
+    const mensagemErro = document.getElementById("mensagemErro");
 
     //validação de dados
     if(isNaN(peso) || isNaN(altura)){
-        alert("Valores inseridos incorretamente. Por favor, insira valores positivos para peso e altura.");
+        mensagemErro.textContent = "Por favor, insira valores numéricos válidos para peso e altura.";
         return;
     }
-    
     if(altura <= 0 || peso <= 0){
-        alert("Valores inseridos incorretamente. Por favor, insira valores positivos tanto para peso quanto para altura.");
+        mensagemErro.textContent = "Por favor, insira valores positivos tanto para peso quanto para altura.";
         return;
 
     }
-
+    
     const imc = peso / (altura * altura);
-    alert("seu IMC é: " + imc.toFixed(2));
+    mensagemErro.style.color = "green";
+    mensagemErro.textContent = `Seu IMC é ${imc.toFixed(2)}`;
     
 
+
     
+
 }
 
