86cebe03e4f7757a4dbd3e83418c61fe7d42c1f5
Jamerson Lucas Tenorio Valentim <jamersontenorio88@gmail.com>
2025-12-22 18:10:56 -0300

    primeira implementacao de js no modelo de obtencao de resultados vindo do usuario

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,28 @@
 alert("Olá, bem-vindo à calculadora de IMC!");
-function calcularIMC() {
-    var peso = parseFloat(document.getElementById("peso").value);
-    var altura = parseFloat(document.getElementById("altura").value) / 100;
+function calcularIMC(event) {
+
+    // Previne o comportamento padrão do formulário
+    event.preventDefault();
+
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+
+    //validação de dados
+    if(isNaN(peso) || isNaN(altura)){
+        alert("Valores inseridos incorretamente. Por favor, insira valores positivos para peso e altura.");
+        return;
+    }
+    
+    if(altura <= 0 || peso <= 0){
+        alert("Valores inseridos incorretamente. Por favor, insira valores positivos tanto para peso quanto para altura.");
+        return;
+
+    }
+
+    const imc = peso / (altura * altura);
+    alert("seu IMC é: " + imc.toFixed(2));
+    
+
+    
 }
+
