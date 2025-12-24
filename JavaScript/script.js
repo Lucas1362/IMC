1eced685f7c1fa822491c7ecabeb68f82691c983
Jamerson Lucas Tenorio Valentim <jamersontenorio88@gmail.com>
2025-12-24 11:44:02 -0300

    aparicao da caixa de mensagem de exibicao de resultados

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,12 +13,17 @@ function calcularIMC(event) {
     function mostrarMensagem(texto, cor = "white") {
         mensagemErro.textContent = texto;
         mensagemErro.style.color = cor;
-        caixaDeErro.style.display = "block";
+        //caixaDeErro.style.display = "block";
 
+
+
+        caixaDeErro.classList.add("show");
 
         setTimeout(() => {
-            caixaDeErro.style.display = "none";
-        }, 5000);
+            caixaDeErro.classList.remove("show");
+        }, 10000);
+       
+
         //validação de dados
     }
     if(isNaN(peso) || isNaN(altura)){
@@ -35,6 +40,7 @@ function calcularIMC(event) {
     mostrarMensagem(`Seu IMC é ${imc.toFixed(2)}`, "white");
     pesoId.textContent = "Digite seu peso (kg)";
     alturaId.textContent = "Digite sua altura (m)";
+
  
 }
 
